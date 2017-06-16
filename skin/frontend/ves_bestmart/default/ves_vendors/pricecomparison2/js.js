4b3da8cc17f6696247deaccd21d30414096792ad
divyanshu <divyanshu.mishmarket@gmail.com>
2017-06-16 13:19:12 +0530

    skin folder

---
+++ skin/frontend/ves_bestmart/default/ves_vendors/pricecomparison2/js.js
@@ -0,0 +1,8 @@
+if(typeof(Product.ConfigurableSwatches) !='undefined'){
+	Product.ConfigurableSwatches.prototype.onOptionClickOrigin = Product.ConfigurableSwatches.prototype.onOptionClick;
+	Product.ConfigurableSwatches.prototype.onOptionClick = function(attr){
+		this.onOptionClickOrigin(attr);
+		
+		priceComparisonTable.render();
+	};
+}
